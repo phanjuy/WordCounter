70cecb378288d953969f4744ae43266b9cead6b7
Duy Phan <phanjuy@hotmail.com>
2024-02-24 17:09:45 +0100

    Add unit test for WriteToFile method

--- WordCounter.UnitTests/WordHistogramTests.cs
+++ WordCounter.UnitTests/WordHistogramTests.cs
@@ -37,4 +37,38 @@ public class WordHistogramTests
         // Assert
         actual.Should().BeEquivalentTo(expected);
     }
+
+    [Fact]
+    public void WriteToFile_ShouldCreateNewFile_GivenValidInput()
+    {
+        // Arrange
+        var inputPath = @"c:\root\in\test.txt";
+        var outputPath = @"c:\root\out\test.txt";
+        var fileSystem = new MockFileSystem();
+        fileSystem.AddDirectory(@"c:\root\out\");
+
+        var histogram = new Dictionary<string, int>()
+        {
+            ["do"] = 4,
+            ["the"] = 2,
+            ["things"] = 2,
+            ["you"] = 2,
+            ["so"] = 2,
+            ["well"] = 2,
+        };
+
+        var expected = "do: 4\r\nso: 2\r\nthe: 2\r\nthings: 2\r\nwell: 2\r\nyou: 2\r\n";
+
+        var sut = new WordHistogram(inputPath, outputPath, fileSystem);
+
+        // Act
+        sut.WriteToFile(histogram);
+
+        // Assert
+        fileSystem.FileExists(outputPath).Should().BeTrue();
+
+        MockFileData outputFile = fileSystem.GetFile(outputPath);
+        outputFile.TextContents.Should().BeEquivalentTo(expected);
+    }
+
 }
